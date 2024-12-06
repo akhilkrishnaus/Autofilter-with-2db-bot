892aaf668c3f67047a44e0044987b7327417f933
akhilkrishnaus <akhilkrishnausa50@gmail.com>
2024-12-06 21:07:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,7 +87,7 @@ async def start(client, message):
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
                     InlineKeyboardButton('• Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ •', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-               ],[
+                ],[
                     InlineKeyboardButton('• Hᴇʟᴘ •', callback_data='help'),
                     InlineKeyboardButton('• Aʙᴏᴜᴛ •', callback_data='about'),
                 ]]
